c04724baf0ccb6b7cdfc50e7ad89bba5df296c7d
sisttara <92781894+SISTTARA09@users.noreply.github.com>
2024-03-27 09:56:34 +0000

    remove env vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	env: {
-		MONGODB_URI: "mongodb://localhost:27017/sample-open-fst",
-		CLOUDINARY_NAME: "dgmichvn8",
-		CLOUDINARY_API_KEY: "312291871695631",
-		CLOUDINARY_API_SECRET: "kLaJytlUKD15DOJpQt1-BYfMOF8",
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
